67d1004da356e56eeb6e7e2e7eef1f9a28ee0b69
Ben Kearns <rapiidz1@gmail.com>
2019-04-10 13:46:12 +0100

    wrote tests for fizzbuzz

--- src/Menu.java
+++ src/Menu.java
@@ -1,7 +1,7 @@
-import java.util.Scanner;
+import java.util.Scanner; // import required to take user input
 
 class Menu { // this class will be used to display the menu to the user
-    public static Scanner in = new Scanner(System.in);
+    public static Scanner in = new Scanner(System.in); // setup to take user input
 
     public static void main(String[] args) {Menu.menu();} // calls the menu method when the program starts
 
@@ -15,7 +15,7 @@ class Menu { // this class will be used to display the menu to the user
         String input = in.nextLine(); // takes the user input
 
         switch(input) {
-            case "1": System.out.println("you entered 1"); 
+            case "1": System.out.println("you entered 1");
                       break;
             case "2": System.exit(0); // closes program if user selects option 2
                       break;
--- test/FizzBuzzTest.java
+++ test/FizzBuzzTest.java
@@ -4,10 +4,37 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest { // this class is used to test the fizzbuzz class
 
-    private FizzBuzz fizzbuzz;
+    // private FizzBuzz fizzbuzz;
 
     @Test
-    public void firstTest() {
+    public void multipleOfThree() {
+
+        String outcome = FizzBuzz.calculation(6);
+        assertEquals(outcome, "Fizz");
+
+    }
+
+    @Test
+    public void multipleOfFive() {
+
+        String outcome = FizzBuzz.calculation(10);
+        assertEquals(outcome, "Buzz");
+
+    }
+
+    @Test
+    public void multipleOfThreeAndFive() {
+
+        String outcome = FizzBuzz.calculation(15);
+        assertEquals(outcome, "FizzBuzz");
+
+    }
+
+    @Test
+    public void NotAMultiple() {
+
+        int outcome = FizzBuzz.calculation(2);
+        assertEquals(outcome, 2);
 
     }
 
